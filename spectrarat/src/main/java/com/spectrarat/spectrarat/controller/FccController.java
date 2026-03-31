39412ed7b77c6a6f01493d2ad90bc1d61d37e4aa
Brady <bratzl3@wgu.edu>
2026-03-31 09:54:59 -0400

    updating fcc controller to have cross origin, everything tested and working to this point (prior to algorithm)

--- spectrarat/src/main/java/com/spectrarat/spectrarat/controller/FccController.java
+++ spectrarat/src/main/java/com/spectrarat/spectrarat/controller/FccController.java
@@ -2,6 +2,7 @@ package com.spectrarat.spectrarat.controller;
 
 import com.spectrarat.spectrarat.model.FrequencyBand;
 import com.spectrarat.spectrarat.service.FccApiService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import reactor.core.publisher.Mono;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200") // Patch: Enables Angular access to FCC data
 @RestController
 @RequestMapping("/api/fcc")
 public class FccController {
