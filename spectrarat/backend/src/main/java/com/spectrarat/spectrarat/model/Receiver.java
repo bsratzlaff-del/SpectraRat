89ab4b249ed9389a9d4b7def477c68e3715ada99
bratzl3@wgu.edu <bratzl3@wgu.edu>
2026-04-06 14:17:34 -0400

    take out json ignore

--- spectrarat/backend/src/main/java/com/spectrarat/spectrarat/model/Receiver.java
+++ spectrarat/backend/src/main/java/com/spectrarat/spectrarat/model/Receiver.java
@@ -3,7 +3,6 @@ package com.spectrarat.spectrarat.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
